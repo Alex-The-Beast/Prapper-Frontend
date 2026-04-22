e61e6c9386a0730a4e5c5f3b32f2ae284043bbe3
Gaurav kumar <gk064577@gmail.com>
2026-04-23 04:29:10 +0530

    error fixing

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
+console.log("API URL:", import.meta.env.VITE_BACKEND_API);
+
 export default axios.create({
+    
     baseURL:import.meta.env.VITE_BACKEND_API,
     headers:{
         "Content-Type":"application/json",
